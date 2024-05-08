0b1c9a8bda495094c34bcd013af83cf2fb7c08d1
MaksimKislyakov <kislakov.maksim.vyacheslavovich@gmail.com>
2024-05-08 22:52:08 +0500

    save

--- app/src/main/java/com/projectpracticum/bucketlist/MainActivity.kt
+++ app/src/main/java/com/projectpracticum/bucketlist/MainActivity.kt
@@ -1,11 +1,21 @@
-package com.projectpracticum.bucketlist
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.projectpracticum.bucketlist.PopularActivity
+import com.projectpracticum.bucketlist.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_kremlin = findViewById<ImageButton>(R.id.btn_kremlin)
+
+        btn_kremlin.setOnClickListener {
+            val intent = Intent(this, PopularActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
